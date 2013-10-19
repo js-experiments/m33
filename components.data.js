870d575a28921257bb78ca4289865300e68e38a3
k33g <ph.charriere@gmail.com>
2013-10-19 09:20:20 +0200

    update name of Underscore

--- components.data.js
+++ components.data.js
@@ -1,6 +1,6 @@
 [
         {"id":"001", "name":"Backbone AMD", "version":"1.1.0"}
-    ,   {"id":"002", "name":"Underscore", "version":"1.5.2"}
+    ,   {"id":"002", "name":"Underscore AMD", "version":"1.5.2"}
     ,   {"id":"003", "name":"jQuery", "version":"2.0.3"}
     ,   {"id":"004", "name":"Require", "version":"2.1.8"}
     ,   {"id":"005", "name":"Require-text", "version":"2.0.10"}
